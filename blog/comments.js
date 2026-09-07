30c5f92923c434f5e0ae70843072808b17d7ac22
Spencer Guo <spencerchenguo@gmail.com>
2026-09-06 21:57:44 -0500

    chore: formatting with Prettier

--- blog/comments.js
+++ blog/comments.js
@@ -1,10 +1,9 @@
-var post= document.getElementById("post");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
- 
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
- 
+var post = document.getElementById("post");
+post.addEventListener("click", function () {
+  var commentBoxValue = document.getElementById("comment-box").value;
+
+  var li = document.createElement("li");
+  var text = document.createTextNode(commentBoxValue);
+  li.appendChild(text);
+  document.getElementById("unordered").appendChild(li);
 });
